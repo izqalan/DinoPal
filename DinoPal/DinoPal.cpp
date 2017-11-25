759fd339fae0d310e8745ec4895fd58eccdc5415
izqalan <izqalan@gmail.com>
2017-11-25 16:36:37 +0800

    initial commit

---
+++ DinoPal/DinoPal.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "DinoPal.h"
+using namespace std;
+
+struct invoice{
+	int Duration;
+	char CurrMonth[10];
+};
+
+struct customer{
+	char CustName[30];
+	char CustNo[14];
+}invoice;
+
+int main()
+{
+
+	return 0;
+}
---
+++ DinoPal/DinoPal.h
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int getCaller()
+{
+	// get user details
+}
+
+int CalculateRate()
+{
+	// calculate inputs
+}
+
+int TotalAmount()
+{
+	// display bills
+}
