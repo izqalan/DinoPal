b9c1c44b5d0a30bc1a97afe5925fcae9c18b6dc8
izqalan <izqalan@gmail.com>
2018-01-02 17:40:04 +0800

    added fileio

--- DinoPal/DinoPal.cpp
+++ DinoPal/DinoPal.cpp
@@ -1,31 +1,38 @@
 #include <iostream>
-#include <fstream>
+#include <windows.h>
 #include "DinoPal.h"
 
 using namespace std;
+/*Documentation is as important as the codes itself */
 
 int main()
 {
-	cout << "=== Thank you for subscribing to DinoPal telco ===" << endl;
-	cout << "===== Please enter your credentials =====" << endl;
-
+	cout << "\t\t\t=== Thank you for subscribing to DinoPal telco ===" << endl;
+	cout << "\t\t\t===== Please enter your credentials =====" << endl;
+	path();
 	// class declaration 
 	userInputs newinput;
 	double *total = NULL;
 	total = new double;
 
-	// func declaration of class userinputs
 	// get user input from public class function
 	struct invoice userdetails;
+	
 	newinput.getInput(&userdetails);
 
 	// referencing customer struct as userdetails && display user input
+	for (int i = 0; i < 3; i++)
+	{
+		Sleep(800);
+		cout << "\t\t\t\t\tLoading..." << endl;
+	}Sleep(500);
 	userDat(&userdetails);
 
 	// display total amount
 	*total = CalculateRate(&userdetails.Duration);
-	
+	Sleep(1000);
 	TotalAmount(&userdetails, total);
+	reciept(&userdetails);
 
 	system("pause");
 	return 0;
